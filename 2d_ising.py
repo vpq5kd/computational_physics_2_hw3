883260d312724e2d817dd759a52154c236ce0437
sophia spaner <vpq5kd@virginia.edu>
2026-02-22 06:43:40 -0500

    working version of just metropolis complete

--- 2d_ising.py
+++ 2d_ising.py
@@ -22,22 +22,49 @@ def hamiltonian(spins, J=1):
 def metropolis(N):
     spins = np.random.choice([-1,1], size=(N,N))
     
-    
-    for i in range(1000):
-        random_site_x = rng.integers(N)
-        random_site_y = rng.integers(N)
+    temp_array = np.linspace(0.1, 5.1, 100)[::-1]
+    energy_array = []
 
-        right = spins[random_site_y, (random_site_x + 1) % N]
-        down = spins[(random_site_y + 1) % N, random_site_x]
-        left = spins[random_site_y, (random_site_x - 1)%N]
-        up = spins[(random_site_y - 1) % N, random_site_x]
-        neighbors = [up, down, right, left]
-        
-        h_mol = 0
-        for neighbor in neighbors:
-            h_mol += neighbor
+    for T in temp_array:
+        temp_energy_array = []
+        initial_energy = hamiltonian(spins)
+        temp_energy_array.append(initial_energy)
+        for i in range(100000):
+            previous_energy = temp_energy_array[-1]
 
-        sigma_i = spins[random_site_y, random_site_x]
-        delta_e = 2 * sigma_i * h_mol
+            random_site_x = rng.integers(N)
+            random_site_y = rng.integers(N)
 
-        if np.random.rand() < np.exp()
+            right = spins[random_site_y, (random_site_x + 1) % N]
+            down = spins[(random_site_y + 1) % N, random_site_x]
+            left = spins[random_site_y, (random_site_x - 1)%N]
+            up = spins[(random_site_y - 1) % N, random_site_x]
+            neighbors = [up, down, right, left]
+            
+            h_mol = 0
+            for neighbor in neighbors:
+                h_mol += neighbor
+
+            sigma_i = spins[random_site_y, random_site_x]
+            delta_e = 2 * sigma_i * h_mol
+
+            B = 1/T
+            if np.random.rand() < np.exp(-B*delta_e):
+                spins[random_site_y, random_site_x] *= -1
+                temp_energy_array.append(previous_energy+delta_e)
+                continue
+
+            temp_energy_array.append(previous_energy)
+
+        energy_array.append(np.mean(temp_energy_array))    
+
+    return np.array(temp_array), np.array(energy_array)
+
+def main():
+    N = 30
+    metropolis_temp_array, metropolis_energy_array = metropolis(30)
+    plt.figure()
+    plt.plot(metropolis_temp_array, metropolis_energy_array/N**2)
+    plt.show()
+
+main()
